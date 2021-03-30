950a85084fa2973bbebbe255bc9fc42c441b1422
rfortress-cb <rfortress@carbonblack.com>
2021-03-30 16:39:12 -0700

    added new node

---
+++ APIs/Enterprise EDR APIs/Feed Manager API/get-specific-feed.js
@@ -0,0 +1,82 @@
+const https = require('https')
+
+module.exports = function(RED) {
+    function GetSpecificFeedNode(config) {
+        RED.nodes.createNode(this,config);
+
+        // Retrieve the config node
+        this.server = RED.nodes.getNode(config.server);
+
+        var node = this;
+        
+        node.on('input', function(msg) {
+            node.status({
+                text: 'getting feed...',
+                fill: 'grey'
+            })
+            
+            if (!msg.feed_id) {
+                if (msg.payload) { msg.feed_id = msg.payload }
+                else {
+                    node.send([null, body])
+                    console.error(body)
+                    node.status({
+                        text: 'Missing feed_id',
+                        fill: 'red'
+                    })    
+                }
+            }            const options = {
+                hostname: this.server.domain,
+                port: 443,
+                path: `/threathunter/feedmgr/v2/orgs/${this.server.org_key}/feeds/${msg.feed_id}`,
+                method: 'GET',
+                headers: {
+                    'X-Auth-Token': `${this.server.custom_api_key}/${this.server.custom_api_id}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+            
+            const req = https.request(options, res => {
+                // Start an emptry string for the body. This will be built on as we iterate through the response parts.
+                let body = '';
+                
+                // Add the response data to the body
+                res.on('data', data => {
+                    body += data.toString();
+                }).on('end', () => {                    
+                    // A 200 is a success
+                    if (res.statusCode == 200) {
+                        node.status({
+                            text: `statusCode: ${res.statusCode}`,
+                            fill: 'green'
+                        })
+
+                        const schema = JSON.parse(body);
+                        msg.payload = schema;
+                        node.send([msg, null]);
+                    } else {
+                        node.send([null, body])
+                        console.error(body)
+                        node.status({
+                            text: `statusCode: ${res.statusCode}`,
+                            fill: 'red'
+                        })
+                    }
+                })
+            })
+            
+            req.on('error', error => {
+                node.send([null, error])
+                console.error(error)
+                node.status({
+                    text: `statusCode: ${res.statusCode}`,
+                    fill: 'red'
+                })
+            })
+            
+            req.end()
+        })
+    }
+
+    RED.nodes.registerType('get-specific-feed', GetSpecificFeedNode);
+}
